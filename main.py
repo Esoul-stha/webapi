82278422174baf514b0386bad95ee39621557f4e
Esoul-stha <shrestha.ishwor@hotmail.com>
2021-06-10 00:39:02 +0900

    fix using lots of looping inside a single func may cause errors, so modified into the simple way

--- main.py
+++ main.py
@@ -18,55 +18,33 @@ CORS(app)
 # GET /stores
 @app.route('/stores')
 def get_stores():
-# 店舗名と店舗コードに関わるコード
-    load_dotenv()
+# extracting name of store and code from #url
+    url = "#url"
+    get_url = requests.get(url)
+    soup = BeautifulSoup(get_url.text, 'html.parser')
+    headings = soup.findAll('h4')
 
-    home_url = os.environ['URL']
+    all_shop_data = []
 
-    session = HTMLSession()
-
-
-    with open('allshopdata.json','w') as jsonfile:
-        get_url = session.get(home_url)
-
-        get_url.html.render(sleep=1)
-
-        kyushu = get_url.html.xpath('//*[@id="######"]', first=True)
-
-        all_shop_data = []
-
-
-        for all_kyushu_store in kyushu.absolute_links:
-            get_store = requests.get(all_kyushu_store)
-            soup = BeautifulSoup(get_store.text, 'html.parser')
-            all_kyushu_store = soup.findAll('div', attrs = {'class':'shop_list'})
-            
-            for shop_list in all_kyushu_store:
-                link = shop_list.findAll('h4')
-
-                for shop_name in link:
-                    links = shop_name.findAll('a')
-                    for name in links:
-                        store_name = name.text
-                        store_code = name.get('href')
-                        store_code = re.sub('[^0-9]','', store_code)
-                        store_code_int = int(store_code)
-                        shop_data = {"store": store_name, "store_number": '{0:04d}'.format(store_code_int)}
-                        all_shop_data.append(shop_data)
-        json.dump(all_shop_data, jsonfile, indent=4, ensure_ascii=False)
-                
-    all_stores = all_shop_data
-    return jsonify({'stores': all_stores})
+    #getting all the tags inside each h4 
+    for tag in headings:
+        # extracting all the contents of anchor tag
+        for name in tag.findAll('a'):
+            store_name = name.text 
+            store_code = name.get('href')
+            # extracting only numbers from href
+            store_code = re.sub('[^0-9]','', store_code)
+            # converting string into int
+            store_code_int = int(store_code)
+            # arranging the data to show output as json
+            shop_data = {"store": store_name, "store_number": store_code_int)}
+            all_shop_data.append(shop_data)
+    return jsonify({'stores': all_shop_data})
 
 
-
-# ホーム画面にhtmlを読み込んでみる。
-# GET /
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-app.run(port=5000)
+# Configure host and port
+if __name__ == '__main__':
+    app.run(port=5000)
 
 
 
